54b4a40ff2ac6086689cfa0e8491c98a48cb5685
welingtonalmeida98 <walmeida98@gmail.com>
2024-07-14 06:13:43 -0300

    aulas tipos variaveis finalizada

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // ESTUDAR tipos primitivos
+        // ESTUDAR a classe String que representa texto na aplicação
+
+        String meuNome = "WELINGTON ALMEIDA";
+
+        
+       double salarioMinimo = 2500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+       int numero = 5;
+
+       numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
